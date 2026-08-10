b839cfea87587a6e8521513d84262bd3ca1e77de
Lee <alipioml0131@gmail.com>
2026-08-10 13:45:34 +0800

    feat: add Prettier configuration and integrate with ESLint for improved code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ['js/recommended', eslintConfigPrettier],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   {
